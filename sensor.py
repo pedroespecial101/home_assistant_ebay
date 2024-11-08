c573f3f44b9dc76c4eb5e3bd71513119150ce6e4
Peter Treadaway <128326013+pedroespecial101@users.noreply.github.com>
2024-11-08 20:15:27 +0000

    Rename eBay sensor from 'ebay_beta_faultChk' to 'ebay'

--- sensor.py
+++ sensor.py
@@ -38,7 +38,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         hass,
         _LOGGER,
         # Name of the data. For logging purposes.
-        name="ebay_beta_faultChk",
+        name="ebay",
         update_method=async_update_data,
         # Polling interval. Will only be polled if there are subscribers.
         update_interval=timedelta(minutes=5),
